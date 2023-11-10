120972607698ced4fec599b3529673b988ef165d
Clupai8o0 <clupaio4@gmail.com>
2023-11-10 19:01:06 +0300

    now it should work

--- app/(Content)/venue/[id]/page.tsx
+++ app/(Content)/venue/[id]/page.tsx
@@ -8,10 +8,13 @@ import Markdown from "@/components/content/Markdown";
 import Package from "@/components/content/Package";
 
 async function getVenue(id: string) {
-	const resp = await fetch(`${process.env.NEXT_PUBLIC_SERVER}api/venue?id=${id}`, {
-		// cache: process.env.status === "DEV" ? "no-store" : "default",
-		cache: "no-store",
-	});
+	const resp = await fetch(
+		`${process.env.NEXT_PUBLIC_SERVER}api/venue?id=${id}`,
+		{
+			// cache: process.env.status === "DEV" ? "no-store" : "default",
+			cache: "no-store",
+		}
+	);
 	const data = await resp.json();
 	return data.data;
 }
@@ -21,37 +24,36 @@ async function Venue({ params: { id } }: { params: { id: string } }) {
 
 	//todo need to make a way to handle the situation where the venue doesn't exist
 	// rather simple but im lazy so
-	// if (!venue) throw new Error("Given venue doesn't exist");
+	if (!venue) throw new Error("Given venue doesn't exist");
 
 	//todo: also for mobile view, need a way to quickly access the package part, table of content?
 	//todo: also level this up like airbnb's got
 	return (
 		<main className="main">
-			{JSON.stringify(venue)}
-			{/* <Gallery imgs={venue.imgs} />
+			<Gallery imgs={venue.imgs} />
 
-			<div className="flex flex-col lg:flex-row gap-8"> */}
+			<div className="flex flex-col lg:flex-row gap-8">
 				{/* //todo: missing an average review and other details */}
-				{/* <div className="w-full md:w-2/3">
-					<h1 className="title mt-12 mb-6">{venue.title}</h1> */}
-					{/* <Profile name={venue.owner.name} title={venue.owner.title} /> */}
+				<div className="w-full md:w-2/3">
+					<h1 className="title mt-12 mb-6">{venue.title}</h1>
+					<Profile name={venue.owner.name} title={venue.owner.title} />
 
-					{/* <Markdown content={venue.desc} />
+					<Markdown content={venue.desc} />
 
 					<h1 className="heading mt-12 mb-6">Location</h1>
 					<Map {...venue.location} />
 
 					<Reviews reviews={venue.reviews} />
 				</div>
-				<div className="w-full md:w-1/3 pt-12"> */}
+				<div className="w-full md:w-1/3 pt-12">
 					{/* //todo: need to make this mobile easy to use */}
-					{/* <Package {...venue.package} />
+					<Package {...venue.package} />
 				</div>
-			</div> */}
+			</div>
 		</main>
 	);
 }
 
 export default Venue;
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
